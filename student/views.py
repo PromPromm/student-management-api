64d0326251866c40a16938638b813b497de99ac8
Falaye Promise <promiseanuoluwa@gmail.com>
2023-03-08 11:37:16 +0100

    started adding functionalities to the student routes

--- student/views.py
+++ student/views.py
@@ -1,24 +1,35 @@
 from flask.views import MethodView
 from flask_smorest import Blueprint
+from flask_jwt_extended import jwt_required
+from models.user import User
+from schemas import UserSchema
+from utils import db
 
 blp = Blueprint("students", __name__, description='Operations on students')
 
 @blp.route("/student")
 class StudentList(MethodView):
+    @blp.response(200, UserSchema(many=True))
+    @jwt_required()
     def get(self):
         """
         Get all students
         """
-        pass
+        students = User.query.filter(User.student_id != "null")
+        return students
 
 
 @blp.route('/student/<int:student_id>')
 class Student(MethodView):
+    @blp.response(200, UserSchema)
+    @jwt_required()
     def get(self, student_id):
         """
         Get a student by id
         """
-        pass
+        student = User.get_by_id(student_id)
+        return student
+
 
     def patch(self, student_id):
         """
@@ -26,11 +37,17 @@ class Student(MethodView):
         """
         pass
 
+    @jwt_required()
     def delete(self, student_id):
         """
-        Delete a student
+        Delete a student by id
         """
-        pass
+        student = User.get_by_id(student_id)
+
+        db.session.delete(student)
+        db.session.commit()
+
+        return {"message": "Student deleted"}
 
 
 @blp.route('/student/<int:student_id>/cgpa')
