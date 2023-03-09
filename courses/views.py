5584409c82b12e3b311e19d330d04daf9a239b20
Falaye Promise <promiseanuoluwa@gmail.com>
2023-03-09 02:41:25 +0100

    Started developing course view functions

--- courses/views.py
+++ courses/views.py
@@ -1,36 +1,49 @@
 from flask.views import MethodView
 from flask_smorest import Blueprint
+from flask_jwt_extended import jwt_required
+from models.courses import Course
+from schemas import PlainCourseSchema
+from flask import abort
 
 blp = Blueprint("courses", __name__, description='Operations on courses')
 
 
 @blp.route("/course")
 class CourseList(MethodView):
+    @jwt_required()
+    @blp.response(200, PlainCourseSchema(many=True))
     def get(self):
         """
         Get all courses
         """
-        pass
+        courses = Course.query.all()
+        return courses
+    
 
-    def post(self):
+    @jwt_required()
+    @blp.arguments(PlainCourseSchema)
+    @blp.response(201, PlainCourseSchema)
+    def post(self, course_data):
         """
         Create a course
         """
-        pass
+        course = Course.query.filter_by(name=course_data['name']).first()
+        if course:
+            abort(401, "Course with that name exists")
+        new_course = Course(name=course_data["name"], teacher=course_data["teacher"])
+        new_course.save()
+        return new_course
 
 @blp.route("/course/<int:course_id>")
-class Course(MethodView):
+class CoursebyId(MethodView):
+    @blp.response(200, PlainCourseSchema)
+    @jwt_required()
     def get(self, course_id):
         """
         Get course by id
         """
-        pass
-
-    def delete(self, course_id):
-        """
-        Delete a course by id
-        """
-        pass
+        course = Course.get_by_id(course_id)
+        return course
 
 
 @blp.route("/course/<int:course_id>/enroll")
